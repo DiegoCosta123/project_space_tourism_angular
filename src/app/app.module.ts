ad75ce576122b8bb06f76b621f6691c94a1d165b
Diego da Costa <diegoccpinto@gmail.com>
2023-05-30 11:06:23 -0300

    Created new header in menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DestinationComponent } from './pages/destination/destination.component';
+import { NavHeaderComponent } from './components/nav-header/nav-header.component';
+import { NavFooterComponent } from './components/nav-footer/nav-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DestinationComponent
+    DestinationComponent,
+    NavHeaderComponent,
+    NavFooterComponent
   ],
   imports: [
     BrowserModule,
